45710a161732841364da6abd3587577b6f60864f
bouzuya <m@bouzuya.net>
2015-12-22 08:38:02 +0900

    remove Observable.prototype.fromEventPattern selector args

--- src/core/linq/observable/fromeventpattern.js
+++ src/core/linq/observable/fromeventpattern.js
@@ -1,25 +1,20 @@
   var EventPatternObservable = (function(__super__) {
     inherits(EventPatternObservable, __super__);
-    function EventPatternObservable(add, del, fn) {
+    function EventPatternObservable(add, del) {
       this._add = add;
       this._del = del;
-      this._fn = fn;
       __super__.call(this);
     }
 
-    function createHandler(o, fn) {
+    function createHandler(o) {
       return function handler () {
         var results = arguments[0];
-        if (isFunction(fn)) {
-          results = tryCatch(fn).apply(null, arguments);
-          if (results === errorObj) { return o.onError(results.e); }
-        }
         o.onNext(results);
       };
     }
 
     EventPatternObservable.prototype.subscribeCore = function (o) {
-      var fn = createHandler(o, this._fn);
+      var fn = createHandler(o);
       var returnValue = this._add(fn);
       return new EventPatternDisposable(this._del, fn, returnValue);
     };
@@ -45,9 +40,8 @@
    * Creates an observable sequence from an event emitter via an addHandler/removeHandler pair.
    * @param {Function} addHandler The function to add a handler to the emitter.
    * @param {Function} [removeHandler] The optional function to remove a handler from an emitter.
-   * @param {Function} [selector] A selector which takes the arguments from the event handler to produce a single item to yield on next.
    * @returns {Observable} An observable sequence which wraps an event from an event emitter
    */
-  var fromEventPattern = Observable.fromEventPattern = function (addHandler, removeHandler, selector) {
-    return new EventPatternObservable(addHandler, removeHandler, selector).publish().refCount();
+  var fromEventPattern = Observable.fromEventPattern = function (addHandler, removeHandler) {
+    return new EventPatternObservable(addHandler, removeHandler).publish().refCount();
   };
